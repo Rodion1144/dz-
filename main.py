e9a04d076f3a918d9b40dd0a626ff0ebab4a4538
Rodion1144 <chistyakov-rodion@bk.ru>
2024-10-15 16:49:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-a = 5
-print(a)
+def get_matrix(n, m, value):
+ matrix = []
+ for i in range(n):
+ row = []
+ for j in range(m):
+ row.append(value)
+ matrix.append(row)
+ return matrix
+
+
+result1 = get_matrix(n = 2, m = 5, value = 5)
+result2 = get_matrix(n = 5, m = 8, value = 19)
+result3 = get_matrix(n = 25, m = 4, value = 44)
+
+print(result1)
+print(result2)
+print(result3)
